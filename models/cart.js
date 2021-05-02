99c8244c7840b7468577b79bd2abde2596b6c67d
Akshay Bengani <akshaybengani@gmail.com>
2021-05-02 15:05:59 +0530

    Added a bug fix

--- models/cart.js
+++ models/cart.js
@@ -41,28 +41,31 @@ module.exports = class Cart {
             });
         });
     }
-    
+
     static deleteProduct(id, productPrice) {
         fs.readFile(p, (err, fileContent) => {
-            if(err) {
+            if (err) {
                 // if there is nothing in the cart or not the item we are looking for
                 return;
             }
-            const updatedCart = {...JSON.parse(fileContent)};
+            const updatedCart = { ...JSON.parse(fileContent) };
             const product = updatedCart.products.find(prod => prod.id === id);
+            if (!product) {
+                return;
+            }
             const productQty = product.qty;
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
             updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
             fs.writeFile(p, JSON.stringify(updatedCart), err => {
                 console.log(err);
             });
-        } );
+        });
     }
 
-    static getProducts(cb){
+    static getProducts(cb) {
         fs.readFile(p, (err, fileContent) => {
             const cart = JSON.parse(fileContent);
-            if(err) {
+            if (err) {
                 cb(null);
             } else {
                 cb(cart);
