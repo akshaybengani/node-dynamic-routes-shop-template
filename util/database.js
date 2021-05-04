f6b98ee0dc8c1317140cec8d676fdc5493aa2f68
Akshay Bengani <akshaybengani@gmail.com>
2021-05-04 09:29:38 +0530

    Transformed raw mysql2 to sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,13 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '3623wxci'
-});
+const sequelize = new Sequelize(
+    'node-complete',
+    'root',
+    '3623wxci',
+    {
+        dialect: 'mysql',
+        host: 'localhost'
+    },
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
